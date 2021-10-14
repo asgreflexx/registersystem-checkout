17cc48015177d32891a68f038fbfbdb1ff9f0e67
Michael Svoboda <michael.svoboda1@stud.fh-campuswien.ac.at>
2021-10-14 20:36:46 +0200

    #1 call backend & add positions to cart

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -2,24 +2,23 @@
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <div class="col-md-12 col-sm-12">
+        <!--<div class="col-md-12 col-sm-12">
           <a href="" role="button" class="btn btn-default btn-neue-rechnung">Neue Rechnung</a>
-        </div>
+        </div>-->
         <div class="col-md-12 col-sm-12">
           <table>
             <tbody>
               <tr>
                 <td>
-                  <form action="" enctype="multipart/form-data" method="post" name="buchen">
-                    <div class="form-row align-items-center">
-                      <div class="col-auto">
-                        <input type="text" class="form-control form-control-lg form-control mb-2" name="code" value="" size="80" autofocus="">
-                      </div>
-                      <div class="col-auto add-to-cart">
-                        <button type="submit" class="btn btn-primary mb-2" name="+" value="+"><i class="fas fa-cart-plus"></i></button>
-                      </div>
+                  <div class="form-row align-items-center">
+                    <div class="col-auto">
+                      <input type="text" class="form-control form-control-lg form-control mb-2" name="code" value="" size="80" autofocus="" v-model="code" v-on:keyup.enter="getProductByCode(code)">
                     </div>
-                  </form>
+                    <div class="col-auto add-to-cart">
+                      <button type="" class="btn btn-primary mb-2" name="+" value="+"
+                              v-on:click="getProductByCode($data.code)"><i class="fas fa-cart-plus"></i></button>
+                    </div>
+                  </div>
                 </td>
               </tr>
             </tbody>
@@ -35,19 +34,21 @@
             </tr>
             </thead>
             <tbody>
-
+            <tr v-for="position in positions" :key="position.name">
+              <td>{{position.name}} </td>
+              <td>1€</td>
+              <td><button role="button" class="btn btn-default btn-neue-rechnung">Löschen</button></td>
+            </tr>
             <tr>
               <th><b>Summe:</b></th>
               <th> €</th>
             </tr>
-<!--            <tr>
-              <td><h3> Mit Karte bezahlen!</h3></td>
-              <td><a href=""><i class="far fa-credit-card"></i></a></td>
-            </tr>-->
-
             </tbody>
           </table>
 
+        </div>
+        <div class="col-md-12 col-sm-12">
+          <a href="" role="button" class="btn btn-default btn-neue-rechnung">Rechnung buchen</a>
         </div>
       </div>
     </div>
@@ -55,13 +56,29 @@
 </template>
 
 <script>
+
 export default {
   name: 'Checkout',
   props: {
     msg: String
   },
+  data: function() {
+    return {
+      responseAvailable: false,
+      code: '',
+      product: '',
+      positions: [],
+  };
+},
   methods: {
-
+    async getProductByCode(code) {
+      try {
+        const response = await this.$http.get('http://localhost:8081/product/code/' + code);
+        this.positions.push(response.data);
+      } catch(error) {
+        console.log(error);
+      }
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import singleSpaVue from 'single-spa-vue';
+import axios from 'axios';
 
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
 
 const vueLifecycles = singleSpaVue({
   Vue,
